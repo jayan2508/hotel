51f99a1859f3f1a531d92af18633173946d29238
jayan <jayanribadiya0812@gmail.com>
2026-06-11 23:40:26 +0530

    completd header

--- src/app/layouts/header/header.ts
+++ src/app/layouts/header/header.ts
@@ -1,21 +1,26 @@
+import { CommonModule } from '@angular/common';
+import { ButtonComponent } from '../../../components/form-controls/button.component/button.component';
 import { Component } from '@angular/core';
-import { InputCompont } from "../../../components/form-controls/input.compont/input.compont";
-import { ButtonComponent } from "../../../components/form-controls/button.component/button.component";
 
 @Component({
   selector: 'app-header',
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
-
-
   pageName = 'Dashboard';
+  selectedTheme = 'light';
+  themeData = [
+    { activeIcon: 'icons/light-active.svg', inactiveIcon: 'icons/light.svg', value: 'light' },
+    { activeIcon: 'icons/dark-active.svg', inactiveIcon: 'icons/dark.svg', value: 'dark' },
+  ];
 
+  constructor() {}
 
+  ngOnInit() {}
 
-
-
-
+  toggleTheme(theme: string) {
+    this.selectedTheme = theme;
+  }
 }
